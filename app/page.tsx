581ccdf4d3d26eafdbd4833b70c1d65f924a19c2
vikashkhati007 <vikashkhati007@gmail.com>
2023-10-03 19:38:15 +0530

    ui fix

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ const page = () => {
   return (
     <div className="w-full h-screen flex flex-col gap-2 justify-center items-center bg-red-500">
       <h1 className="text-3xl text-white font-bold">V-Downloader</h1>
-      <div className="filecontainer border md:w-[60%] w-[90%] h-[80%] rounded-lg flex flex-col gap-10 justify-center items-center bg-gradient-to-br from-[#F0F0F0] to-[#F1EFEF] shadow">
+      <div className="filecontainer border md:w-[60%] w-[80%] h-[60%] rounded-lg flex flex-col gap-10 justify-center items-center bg-gradient-to-br from-[#F0F0F0] to-[#F1EFEF] shadow">
         <Input onChange={getUrl} value={url} />
         {!hidden ? (
           <Button onclick={getDownload} text={text} />
