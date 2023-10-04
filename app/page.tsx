f6769b710608134c0f0d3608657f92f2f93f4f97
vikashkhati007 <vikashkhati007@gmail.com>
2023-10-04 18:27:43 +0530

    youtube added

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import React, { useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 
-const page = () => {
+const Page = () => {
   const [url, setUrl] = useState("");
-  const [download, setdownload] = useState("");
-  const [hidden, sethidden] = useState(false);
+  const [download, setDownload] = useState("");
+  const [hidden, setHidden] = useState(false);
   const [text, setText] = useState("SUBMIT");
 
   interface PropsType {
@@ -21,85 +21,78 @@ const page = () => {
     setUrl(e.target.value);
   };
 
-  if(url === ""){
-    return setText("Wroung URL")
-  }
-
   const getDownload = async () => {
     setText("Please Wait ...");
 
     if (url.includes("facebook")) {
-      const FacebookOption = {
-        method: "GET",
-        url: "https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php",
-        params: {
-          url: url,
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "6eab60dbd1mshb3fc9aaa2e46d43p1858bajsncb1d0d099d75",
-          "X-RapidAPI-Host":
-            "facebook-reel-and-video-downloader.p.rapidapi.com",
-        },
-      };
-
-      const fbresponse = await axios.request(FacebookOption);
-      if (fbresponse) {
-        sethidden(true);
-        setdownload(fbresponse.data.links["Download High Quality"]);
-      } else {
-        console.log("data not fetched");
-      }
-    }
-
-    if (url.includes("instagram")) {
-      const InstagramOptions = {
-        method: "GET",
-        url: "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index",
-        params: {
-          url: url,
-        },
-        headers: {
-          "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_KEY,
-          "X-RapidAPI-Host":
-            "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com",
-        },
-      };
-
-      const instaresponse = await axios.request(InstagramOptions);
-      if (instaresponse) {
-        sethidden(true);
-        setdownload(instaresponse.data.media);
-      } else {
-        console.log("data not fetched");
-      }
-    }
-
-    if (url.includes("youtube")) {
-      const options = {
-        method: 'GET',
-        url: 'https://youtube-audio-video-download.p.rapidapi.com/geturl',
-        params: {
-          video_url: url
-        },
-        headers: {
-          'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_KEY,
-          'X-RapidAPI-Host': 'youtube-audio-video-download.p.rapidapi.com'
-        }
-      };
-      
-        const response = await axios.request(options);
-        if (response) {
-          sethidden(true);
-          console.log(response.data);
+        const FacebookOption = {
+            method: "GET",
+            url: "https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php",
+            params: {
+              url: url,
+            },
+            headers: {
+              "X-RapidAPI-Key":
+                "6eab60dbd1mshb3fc9aaa2e46d43p1858bajsncb1d0d099d75",
+              "X-RapidAPI-Host":
+                "facebook-reel-and-video-downloader.p.rapidapi.com",
+            },
+          };
+    
+          const fbresponse = await axios.request(FacebookOption);
+          if (fbresponse) {
+            setHidden(true);
+            setDownload(fbresponse.data.links["Download High Quality"]);
+          } else {
+            console.log("data not fetched");
+          }
+    } else if (url.includes("instagram")) {
+        const InstagramOptions = {
+            method: "GET",
+            url: "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index",
+            params: {
+              url: url,
+            },
+            headers: {
+              "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_KEY,
+              "X-RapidAPI-Host":
+                "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com",
+            },
+          };
+    
+          const instaresponse = await axios.request(InstagramOptions);
+          if (instaresponse) {
+            setHidden(true);
+            setDownload(instaresponse.data.media);
+          } else {
+            console.log("data not fetched");
+          }
+    } else if (url.includes("youtu.be")) {
+        const encodedParams = new URLSearchParams();
+        encodedParams.set("url", url);
+  
+        const Youtubeoptions = {
+          method: "POST",
+          url: "https://all-media-downloader.p.rapidapi.com/download",
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_KEY,
+            "X-RapidAPI-Host": "all-media-downloader.p.rapidapi.com",
+          },
+          data: encodedParams,
+        };
+  
+        const youtuberesponse = await axios.request(Youtubeoptions);
+        if (youtuberesponse) {
+          setHidden(true);
+          setDownload(youtuberesponse.data['720p'].url);
         } else {
           console.log("data not fetched");
         }
-     
+    } else {
+      setText("Invalid URL");
     }
-
   };
-  
 
   return (
     <div className="w-full h-screen flex flex-col gap-2 justify-center items-center bg-red-500">
@@ -107,7 +100,7 @@ const page = () => {
       <div className="filecontainer border md:w-[60%] w-[80%] h-[60%] rounded-lg flex flex-col gap-10 justify-center items-center bg-gradient-to-br from-[#F0F0F0] to-[#F1EFEF] shadow">
         <Input onChange={getUrl} value={url} />
         {!hidden ? (
-          <Button onclick={getDownload} text={text} />
+          <Button onClick={getDownload} text={text} />
         ) : (
           <Link download={download} href={download}>
             <Button text={"DOWNLOAD"} />
@@ -118,4 +111,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- components/button.tsx
+++ components/button.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({text,onclick}:any) => {
+const Button = ({text,onClick}:any) => {
   return (
     <div>
-      <button onClick={onclick} className='border px-4 py-2 rounded-md bg-red-600 text-white font-bold hover:bg-red-500 shadow'>{text}</button>
+      <button onClick={onClick} className='border px-4 py-2 rounded-md bg-red-600 text-white font-bold hover:bg-red-500 shadow'>{text}</button>
     </div>
   )
 }
